5443156bc6c7170841215931b13306ec204b29be
stereo8 <plavsic.vid@gmail.com>
2022-07-25 07:47:14 +0200

    add prettier

--- src/background.ts
+++ src/background.ts
@@ -5,62 +5,62 @@ import * as Browser from 'webextension-polyfill'
 console.log('hi')
 
 const removeThese = [
-    'official video',
-    'music video',
-    'official audio',
-    'official',
-    'video',
-    'audio',
-    'ft.',
-    'feat.',
-    'featuring',
-    'prod.',
-    'prod',
-    '()',
-    '[]',
-    '-',
-    'x',
+  'official video',
+  'music video',
+  'official audio',
+  'official',
+  'video',
+  'audio',
+  'ft.',
+  'feat.',
+  'featuring',
+  'prod.',
+  'prod',
+  '()',
+  '[]',
+  '-',
+  'x',
 ]
 
 Browser.runtime.onMessage.addListener(async (message, sender) => {
-    let sanitizedName = ''
+  let sanitizedName = ''
 
-    if (message?.artistName) {
-        sanitizedName = Object.values(message).join(' ')
-    } else {
-        sanitizedName = message?.songTitle.toLowerCase()
-        removeThese.forEach((str) => {
-            sanitizedName = sanitizedName.replace(str, '')
-        })
-    }
+  if (message?.artistName) {
+    sanitizedName = Object.values(message).join(' ')
+  } else {
+    sanitizedName = message?.songTitle.toLowerCase()
+    removeThese.forEach((str) => {
+      sanitizedName = sanitizedName.replace(str, '')
+    })
+  }
 
-    await findOnGenius(sanitizedName, sender)
+  await findOnGenius(sanitizedName, sender)
 })
 
 async function findOnGenius(sanitizedName: string, sender: MessageSender) {
-    console.log(sanitizedName)
-    const geniusToken =
+  console.log(sanitizedName)
+  const geniusToken =
     'Bearer PcPAfOlYQSjAYOvfNVHuj4vlHbGxAVzWF8xM8Ifja_fWeDdfjqSG8VwQlNqNJ5mF'
-    const request = new Request(
-        `https://api.genius.com/search?q=${sanitizedName}`,
-        {
-            method: 'GET',
-            headers: {
-                Authorization: geniusToken,
-            },
-            mode: 'cors',
-        }
-    )
-    const response = await fetch(request)
-    const json = await response.json()
-
-    const songURL: string = json?.response?.hits[0]?.result?.url
-
-    if (songURL) {
-        console.log(sender)
-        await Browser.tabs.create({url: songURL})
-        await Browser.tabs.sendMessage(sender.tab.id, { stopSpin: true })
-    } else {
-        await Browser.tabs.sendMessage(sender.tab.id, { shake: true })
+  const request = new Request(
+    `https://api.genius.com/search?q=${sanitizedName}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: geniusToken,
+      },
+      mode: 'cors',
     }
+  )
+  const response = await fetch(request)
+  const json = await response.json()
+
+  const songURL: string = json?.response?.hits[0]?.result?.url
+
+  if (songURL) {
+    console.log(sender)
+    await Browser.tabs.create({url: songURL})
+    await Browser.tabs.sendMessage(sender.tab.id, { stopSpin: true })
+  } else {
+    await Browser.tabs.sendMessage(sender.tab.id, { shake: true })
+  }
 }
